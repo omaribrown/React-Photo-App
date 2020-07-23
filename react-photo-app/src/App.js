3b13b34513c5898c7b47eba007744e865f49e012
Omari Brown <omaris.brown@gmail.com>
2020-07-23 17:13:31 -0400

    completed login css

--- react-photo-app/src/App.js
+++ react-photo-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar'
+import Login from './Components/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Login />
     </div>
   );
 }
--- react-photo-app/src/Components/Login.jsx
+++ react-photo-app/src/Components/Login.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import './Styles/Login.css'
 
 export default class Login extends React.Component {
 
     render() {
         return (
             <div>
-                
+                <Container className='login-container'>
+                    <Row>
+                        <Col>
+                            <Form>
+                                <Form.Group controlId='formGroupEmail'>
+                                    <Form.Label>Username or Email Address</Form.Label>
+                                    <Form.Control type='email' placeholder='Enter Username or Email' className='form-box'/>
+                                </Form.Group>
+                                <Form.Group controlId='formGroupPassword'>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type='password' placeholder='Password' className='form-box'/>
+                                </Form.Group>
+                                <Button variant='primary' type='submit'>
+                                    Login
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
 }
+
+// HARD TO DO 
