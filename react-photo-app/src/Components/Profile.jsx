5cd6ef2ec27886f81c3d8c62347e9dc1d4393ccb
Omari Brown <omaris.brown@gmail.com>
2020-07-24 09:45:59 -0400

    input a tabs layout for the profile page

--- react-photo-app/src/Components/Profile.jsx
+++ react-photo-app/src/Components/Profile.jsx
@@ -4,21 +4,34 @@ import './Styles/Profile.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 
 export default class Profile extends React.Component {
     render() {
         return (
             <div>
-                <Container>
+                <Tabs defaultActiveKey='profile' id ='uncontrolled-tab-example'>
+                    <Tab eventKey='profile' title='Information'>
+                        <h1>profile infooooo</h1>
+                    </Tab>
+                    <Tab eventKey='posts' title='Posts'>
+                        <h1>bookmarks infooooo</h1>
+                    </Tab>
+                    <Tab eventKey='bookmarks' title='Bookmarks'>
+                        <h1>bookmarks infooooo</h1>
+                    </Tab>
+                </Tabs>
+                {/* <Container>
                     <Row>
                         <Col><img src={girl1} alt='girl1' /></Col>
                         <Col><h2>Name</h2></Col>
                     </Row>
                     
-                </Container>
+                </Container> */}
                 
-                <p>Bio</p>
-                <h4>Uploads</h4>
+                {/* <p>Bio</p>
+                <h4>Uploads</h4> */}
                 {/* photo grid */}
             </div>
         )
