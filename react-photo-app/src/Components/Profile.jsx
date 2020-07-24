c157334232c5b8b9c8eea5537a9eff2b6591287c
Omari Brown <omaris.brown@gmail.com>
2020-07-23 20:23:41 -0400

    added pics to use as profile pics for profiles

--- react-photo-app/src/Components/Profile.jsx
+++ react-photo-app/src/Components/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import girl1 from './Images/profile-pics/girl1.jpeg'
 
 export default class Profile extends React.Component {
     render() {
         return (
             <div>
-                
+                <img src={girl1} alt='girl1' />
             </div>
         )
     }
